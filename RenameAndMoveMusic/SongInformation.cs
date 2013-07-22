1989d16ccebeb69c713869f416044286e0fd8895
Andrew Rondeau <github@andrewrondeau.com>
2013-07-21 21:46:49 -0700

    tweaks

--- RenameAndMoveMusic/SongInformation.cs
+++ RenameAndMoveMusic/SongInformation.cs
@@ -9,16 +9,33 @@ namespace RenameAndMoveMusic
 	{
 		public SongInformation (string path)
 		{
-			using (var file = new TagLib.Mpeg4.File(path))
+			try
 			{
-				var tags = file.GetTag(TagLib.TagTypes.Apple) as TagLib.Mpeg4.AppleTag;
+				using (var file = new TagLib.Mpeg4.File(path))
+				{
+					var tags = file.GetTag(TagLib.TagTypes.Apple) as TagLib.Mpeg4.AppleTag;
 
-				this.path = path;
-				this.title = tags.Title;
-				this.album = tags.Album;
-				this.artist = tags.AlbumArtists.FirstOrDefault() ?? tags.Performers.FirstOrDefault();
-				this.track = tags.Track > 0 ? (int?)tags.Track : (int?)null;
-				this.isCompilation = tags.IsCompilation;
+					this.path = path;
+					this.title = tags.Title;
+					this.album = tags.Album;
+					this.artist = tags.AlbumArtists.FirstOrDefault() ?? tags.Performers.FirstOrDefault();
+					this.track = tags.Track > 0 ? (int?)tags.Track : (int?)null;
+					this.isCompilation = tags.IsCompilation;
+				}
+			}
+			catch
+			{
+				using (var file = TagLib.File.Create(path))
+				{
+					var tags = file.GetTag(TagLib.TagTypes.Id3v2);
+
+					this.path = path;
+					this.title = tags.Title;
+					this.album = tags.Album;
+					this.artist = tags.AlbumArtists.FirstOrDefault() ?? tags.Performers.FirstOrDefault();
+					this.track = tags.Track > 0 ? (int?)tags.Track : (int?)null;
+					//this.isCompilation = tags.IsCompilation;
+				}
 			}
 		}
 
